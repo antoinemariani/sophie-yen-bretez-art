83cbd44e596ce0ce6221d5fdd6e1f985b7f7e503
Antoine Mariani <antoine.mariani@me.com>
2023-04-19 17:22:40 +0200

    Fixing typo transition to sans-serif

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,6 @@
 import '@/styles/globals.scss';
 import Layout from '@/components/Layout';
 
-// import localFont from 'next/font/local';
-
-// const libreBaskerville = localFont({
-//   src: [
-//     {
-//       path: './font/LibreBaskerville-Regular.ttf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './font/LibreBaskerville-Italic.ttf',
-//       weight: '400',
-//       style: 'italic',
-//     },
-//     {
-//       path: './font/LibreBaskerville-Bold.ttf',
-//       weight: '700',
-//       style: 'normal',
-//     },
-//   ],
-// });
-
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
