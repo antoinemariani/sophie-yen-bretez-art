ac870d271ab9c89e6233f7b84a0a718f01948c05
Antoine Mariani <antoine.mariani@me.com>
2023-06-06 16:46:19 +0200

    adding useEffect and useState for media queries in Navbar behaviours

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '@/styles/navbar.module.scss';
+import { useState, useEffect } from 'react';
 
 export default function Navbar({}) {
-  return (
+  const [media, setMedia] = useState('null');
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setMedia('desktop');
+    } else {
+      setMedia('mobile');
+    }
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 769px)');
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    setMedia(mediaQuery.matches ? 'desktop' : 'mobile');
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
+  const desktopNavbar = (
     <nav className={styles.navbar}>
       <Link href="/">
         <h1 className={styles['navbar-title']}>sophie-yen bretez</h1>
@@ -34,4 +54,12 @@ export default function Navbar({}) {
       </div>
     </nav>
   );
+
+  if (media === 'desktop') {
+    console.log('desktop');
+    return desktopNavbar;
+  } else if (media === 'mobile') {
+    console.log('mobile');
+    /* the view port is less than 400 pixels wide */
+  }
 }
