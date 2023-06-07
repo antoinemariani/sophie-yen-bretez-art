d434d83120c45233a40b61213ccac92e332087f4
Antoine Mariani <antoine.mariani@me.com>
2023-06-07 10:31:23 +0200

    Refacto prep work for mobile navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar({}) {
   const desktopNavbar = (
     <nav className={styles.navbar}>
       <Link href="/">
-        <h1 className={styles['navbar-title']}>sophie-yen bretez</h1>
+        <h1 className={styles.navbar_title}>sophie-yen bretez</h1>
       </Link>
       <div className={styles.ul}>
         <Link className={styles.li} href="/">
@@ -55,11 +55,42 @@ export default function Navbar({}) {
     </nav>
   );
 
+  const mobileNavbar = (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <h1 className={styles.navbar_title}>mobile</h1>
+      </Link>
+      <div className={styles.ul}>
+        <Link className={styles.li} href="/">
+          Home
+        </Link>
+        <Link className={styles.li} href="/art">
+          Works
+        </Link>
+        <Link className={styles.li} href="/about">
+          About
+        </Link>
+
+        {/* To activate in productions in June 2023 : */}
+        <Link className={styles.li} href="/exhibitions">
+          Exhibitions
+        </Link>
+        <Link className={styles.li_disabled} href="/" disabled={true}>
+          Press
+        </Link>
+        {/* end of unactivated pages */}
+        <Link className={styles.li} href="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+
   if (media === 'desktop') {
-    console.log('desktop');
+    // console.log('desktop');
     return desktopNavbar;
   } else if (media === 'mobile') {
-    console.log('mobile');
-    /* the view port is less than 400 pixels wide */
+    // console.log('mobile');
+    return mobileNavbar;
   }
 }
